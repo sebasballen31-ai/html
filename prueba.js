7b060c6e209812a5bff4fcc2d91b9b2a9878b293
Sebastian Ballen <sebasballen31@gmail.com>
2025-08-28 16:54:33 -0500

    basic html structure created

---
+++ prueba.js
@@ -0,0 +1,19 @@
+setTimeout(() => {
+    console.log('sali de la casa')
+
+    setTimeout(() => {
+        console.log('me fui en bicicleta')
+
+        setTimeout(() => {
+            console.log('y llegue al colegio')
+
+            setTimeout(() => {
+                console.log('y despues me puse a estudiar')
+                setTimeout(() => {
+                    console.log('cuando me encontre con la profesora')
+                }, 4000);
+            }, 2000);
+
+        }, 3000);
+    }, 6000);
+}, 7000);
