47cf45a79f8b839b9de54b9db43e6dcdacba6379
Sebastian Ballen <sebasballen31@gmail.com>
2025-09-23 17:23:53 -0500

    fix: standardize HTML doctype and improve formatting in multiple files

--- prueba.js
+++ prueba.js
@@ -1,19 +1,18 @@
 setTimeout(() => {
-    console.log('sali de la casa')
+    console.log("sali de la casa");
 
     setTimeout(() => {
-        console.log('me fui en bicicleta')
+        console.log("me fui en bicicleta");
 
         setTimeout(() => {
-            console.log('y llegue al colegio')
+            console.log("y llegue al colegio");
 
             setTimeout(() => {
-                console.log('y despues me puse a estudiar')
+                console.log("y despues me puse a estudiar");
                 setTimeout(() => {
-                    console.log('cuando me encontre con la profesora')
+                    console.log("cuando me encontre con la profesora");
                 }, 4000);
             }, 2000);
-
         }, 3000);
     }, 6000);
 }, 7000);
