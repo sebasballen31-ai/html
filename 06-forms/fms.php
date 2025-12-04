2dcc592758985ae1d0820dbf06840805e89dbe7b
Eduard Castellanos <andrescastellanos112013@gmail.com>
2025-12-04 17:05:23 -0500

    feat: db connection

--- 06-forms/fms.php
+++ 06-forms/fms.php
@@ -1,15 +1,23 @@
+<?php include_once "../db.php"; ?>
+<?php
+
+if ($pdo) {
+    $users = $pdo->query("SELECT * FROM public.users")->fetchAll(PDO::FETCH_ASSOC);
+} else {
+    $users = [];
+}
+$i = 1;
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>formularios</title>
         <?php include "../templates/assets.php"; ?>
-
     </head>
 
     <body>
         <?php include "../templates/header.php"; ?>
-
-        <div class="container">
+        <div class="container mb-4">
             <form action="/procesar" method="post">
                 <div class="mb-3">
                     <label for="nombre" class="form-label">Nombre:</label>
@@ -37,55 +45,26 @@
                     Enviar
                 </button>
             </form>
-            <form action="/procesar" method="post">
-                <div class="mb-3">
-                    <label for="nombre" class="form-label">Nombre:</label>
-                    <input
-                        type="text"
-                        id="nombre"
-                        name="nombre"
-                        class="form-control"
-                    />
-                    <div id="passwordHelpBlock" class="form-text">
-                        El nombre debe tener entre 3 y 50 caracteres.
-                    </div>
-                </div>
-                Contraseña:
-                <input type="password" name="clave" class="form-control" />
-
-                Género:
-                <input type="radio" name="genero" value="M" />
-                Masculino
-                <input type="radio" name="genero" value="F" />
-                Femenino Intereses:
-                <input type="checkbox" name="deporte" />
-                Deporte
-                <input type="checkbox" name="musica" />
-                Música <br />
-                <button type="submit">Registrar</button>
-            </form>
-
-            <textarea name="hola" rows="4" cols="20"></textarea>
-
-            <div class="mb-3">
-                <label for="ciudad" class="form-select">Ciudad:</label>
-                <select name="ciudad">
-                    <option value="1">Bogotá</option>
-                    <option value="2">Medellín</option>
-                    <option value="3">Cali</option>
-                </select>
-
-                <select
-                    class="form-select"
-                    multiple
-                    aria-label="Multiple select example"
-                >
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-            </div>
+        </div>
+        <div class="container">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($users as $user): ?>
+                        <tr>
+                            <td><?= $i++ ?></td>
+                            <td><?= $user['name'] ?></td>
+                            <td><?= $user['email'] ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
          <?php include "../templates/footer.php"; ?>
     </body>
--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-<?php
-if (!file_exists('.env')) {
-    die('El archivo .env no existe');
-}
-
-$env = parse_ini_file('.env');
-
-$host = $env['DB_HOST'];
-$port = $env['DB_PORT'];
-$db   = $env['DB_NAME'];
-$user = $env['DB_USER'];
-$pass = $env['DB_PASS'];
-
-$dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
-$pdo = null;
-try {
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 5 // Timeout de 5 segundos
-    ]);
-    // Si deseas probar la conexión, puedes ejecutar una consulta simple
-    // $pdo->query("SELECT 1");
-} catch (PDOException $e) {
-    //
-}
-?>
+<?php include_once "db.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
